c1f9aa5328ff8aef2601bfc856c1fce5a00ba9bd
khanh2911 <khanhduong.291101@gmail.com>
2023-03-16 00:05:14 +0700

    theo nam

--- src/Component/Page/Admin/User/ChartLine.jsx
+++ src/Component/Page/Admin/User/ChartLine.jsx
@@ -11,7 +11,9 @@ import {
 } from "chart.js";
 import { Line } from "react-chartjs-2";
 import axios from "axios";
-import { Spinner } from "flowbite-react";
+import { Card, Dropdown, Label, Spinner } from "flowbite-react";
+import { useDispatch } from "react-redux";
+import { setToken } from "../../../../store/authSlice";
 
 ChartJS.register(
   CategoryScale,
@@ -37,10 +39,25 @@ export const options = {
 };
 
 export function ChartLine() {
+  const dispatch = useDispatch();
   const [chartData, setChartData] = useState(null);
+  const [years, setYears] = useState([]);
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
   useEffect(() => {
     axios
-      .get("/user/chart")
+      .get(`/user/chat/years`)
+      .then((response) => {
+        setYears(response.data);
+      })
+      .catch((error) => {
+        if (error.response.status === 403) {
+          dispatch(setToken(""));
+        }
+      });
+  }, []);
+  useEffect(() => {
+    axios
+      .get("/user/chart", { params: { year: currentYear }})
       .then((response) => response.data)
       .then((data) => {
         console.log(data);
@@ -59,10 +76,43 @@ export function ChartLine() {
           ],
         });
       });
-  }, []);
+  }, [currentYear]);
+  const handelChangeYaer = (a) =>{
+    setCurrentYear(a)
+  }
   return chartData === null ? (
     <Spinner color="failure" />
   ) : (
-    <Line options={options} data={chartData} width={80} height={35} />
+    <Card>
+      <div className="flex justify-between items-center">
+        <Label className="text-xl">Biểu đồ tỉ lệ chat</Label>
+        <Dropdown
+          label={currentYear}
+          style={{ height: "21px", width: "150px" }}
+          color="greenToBlue"
+        >
+          {years.map((year) => (
+            <Dropdown.Item key={year} value={year} onClick={() => handelChangeYaer(year)}>
+              {year}
+            </Dropdown.Item>
+          ))}
+        </Dropdown>
+      </div>
+      <div className="flex">
+        <div
+          className="flex flex-wrap"
+        >
+          <Label>Chỉ tiêu: {chartData.requiredHours} giờ</Label>
+          {chartData.requiredHours -chartData.totalHours <0 &&(
+            <Label className="ml-5">Vượt chỉ tiêu: {chartData.totalHours-chartData.requiredHours} giờ</Label>
+          )}
+          {chartData.missHours !== 0 &&(
+            <Label className="ml-5">Còn thiếu: {chartData.missHours} giờ</Label>
+          )}
+        </div>
+      </div>
+
+          <Line options={options} data={chartData} width={80} height={35} />
+    </Card>
   );
 }
--- src/Component/Page/Lecturer/ChartPie.jsx
+++ src/Component/Page/Lecturer/ChartPie.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Pie } from "react-chartjs-2";
-import { Card, Label } from "flowbite-react";
+import { Card, Dropdown, Label, Spinner } from "flowbite-react";
 import axios from "axios";
 import { useStore } from "react-redux";
 import jwt_decode from "jwt-decode";
@@ -9,19 +9,31 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 export function ChartPie() {
   const [chartData, setChartData] = useState(null);
+  const [years, setYears] = useState([]);
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
   const { token } = useStore().getState().auth;
   const decoded = jwt_decode(token);
   const userId = decoded.id;
   useEffect(() => {
     axios
-      .get(`/user/job/chart-data/${userId}`)
+      .get(`/user/job/chart-data/${userId}/${currentYear}`)
       .then((response) => {
         setChartData(response.data);
       })
       .catch((error) => {
         console.error(error);
       });
-  }, [userId]);
+  }, [userId, currentYear]);
+  useEffect(() => {
+    axios
+      .get(`/user/get/academic-year`)
+      .then((response) => {
+        setYears(response.data.map((a) => a.academicYear));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   const data = {
     labels: ["Giờ còn thiếu", "Giờ hoàn thành"],
@@ -30,7 +42,7 @@ export function ChartPie() {
         label: "# giờ",
         data: chartData
           ? [
-              chartData.requiredHours - chartData.totalHours,
+              chartData.missHours,
               chartData.totalHours,
             ]
           : [0, 0],
@@ -40,15 +52,45 @@ export function ChartPie() {
       },
     ],
   };
-  return (
+  //thay đổi năm
+  const handelChangeYaer = (a) =>{
+    setCurrentYear(a)
+  }
+  return chartData === null ? (
+    <Spinner color="failure" />
+  ) : (
     <Card>
       <div className="flex justify-between items-center">
         <Label className="text-xl">Biểu đồ tỉ lệ giờ hoạt động</Label>
+        <Dropdown
+          label={currentYear}
+          style={{ height: "21px", width: "150px" }}
+          color="greenToBlue"
+        >
+          {years.map((year) => (
+            <Dropdown.Item key={year} value={year} onClick={() => handelChangeYaer(year)}>
+              {year}
+            </Dropdown.Item>
+          ))}
+        </Dropdown>
+      </div>
+      <div className="flex">
+        <div
+          className="flex flex-wrap"
+        >
+          <Label>Chỉ tiêu: {chartData.requiredHours} giờ</Label>
+          {chartData.requiredHours -chartData.totalHours <0 &&(
+            <Label className="ml-5">Vượt chỉ tiêu: {chartData.totalHours-chartData.requiredHours} giờ</Label>
+          )}
+          {chartData.missHours !== 0 &&(
+            <Label className="ml-5">Còn thiếu: {chartData.missHours} giờ</Label>
+          )}
+        </div>
       </div>
       <div className="flex justify-center items-center">
         <div
           className="flex flex-wrap"
-          style={{ width: "500px", height: "410px" }}
+          style={{ width: "500px", height: "375px" }}
         >
           <Pie data={data} className="ml-6" />
         </div>
